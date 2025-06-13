22b5121dac27aeac9abfb5c96fa643c20c6f52ff
nguyennga <nguyennga2617@gmail.com>
2025-06-13 11:05:59 +0700

    code them home

--- src/components/client/home/data-home.js
+++ src/components/client/home/data-home.js
@@ -3,10 +3,68 @@ import dish1 from '../../../assets/client/images/home/dish1.webp';
 import dish2 from '../../../assets/client/images/home/dish2.webp';
 import dish3 from '../../../assets/client/images/home/dish3.webp';
 import dish4 from '../../../assets/client/images/home/dish4.webp';
+import endow1 from "../../../assets/client/images/endow-blog/endow1.png";
+import endow2 from "../../../assets/client/images/endow-blog/endow2.webp";
 
 export const dishes = [
   { id: 1, name: 'Măng trúc xào bò', price: '145,000', img: dish1 },
   { id: 2, name: "Gà H'Mong rang muối", price: '350,000', img: dish2 },
   { id: 3, name: 'Cá chẽm chiên giòn kèm xoài xanh', price: '239,000', img: dish3 },
   { id: 4, name: 'Dê xào hoa chuối', price: '169,000', img: dish4 },
+];
+export const endows = [
+  {
+    id: 1,
+    slug: "tu-hao-viet-nam",
+    description: "TỰ HÀO VIỆT NAM – MỘT DỰ ÁN ĐẶC BIỆT CỦA QUÁN NHẬU TỰ DO",
+    image: endow1,
+  },
+  {
+    id: 2,
+    slug: "mien-phi-ship-cuoi-tuan",
+    description: "MIỄN PHÍ SHIP CUỐI TUẦN - ĐẶT LÀ CÓ, KHÔNG LO PHÍ",
+    image: endow2,
+  },
+  {
+    id: 3,
+    slug: "uu-dai-thanh-vien-moi",
+    description: "ƯU ĐÃI THÀNH VIÊN MỚI - NHẬN NGAY VOUCHER 100K",
+    image: endow1,
+  },
+  {
+    id: 4,
+    slug: "happy-hour-2025",
+    description: "HAPPY HOUR - MUA 1 TẶNG 1 TỪ 17H ĐẾN 19H MỖI NGÀY",
+    image: endow1,
+  },
+  {
+    id: 5,
+    slug: "thu-5-dac-biet",
+    description: "THỨ 5 ĐẶC BIỆT - GIẢM 50% CÁC MÓN ĂN ĐẶC SẮC",
+    image: endow1,
+  },
+  {
+    id: 6,
+    slug: "qua-tang-cho-ban-than",
+    description: "QUÀ TẶNG CHO BẠN THÂN - TẶNG NƯỚC UỐNG MIỄN PHÍ",
+    image: endow1,
+  },
+  {
+    id: 7,
+    slug: "di-3-tinh-tien-2",
+    description: "ĐI 3 TÍNH TIỀN 2 - HỘI NHẬU KHÔNG LO GIÁ",
+    image: endow1,
+  },
+  {
+    id: 8,
+    slug: "buffet-tai-ban",
+    description: "BUFFET TẠI BÀN - CHỈ VỚI 199K",
+    image: endow1,
+  },
+  {
+    id: 9,
+    slug: "khai-truong-uu-dai",
+    description: "KHAI TRƯƠNG - ƯU ĐÃI LÊN ĐẾN 70%",
+    image: endow1,
+  }
 ];
--- src/components/client/home/home.jsx
+++ src/components/client/home/home.jsx
@@ -1,35 +1,22 @@
 import React from 'react';
 import './home.scss';
 import Slider from 'react-slick';
-import { dishes } from './data-home';
+import { dishes, endows } from './data-home';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,      // ← show 4 cards on desktop
+    slidesToShow: 4,      // 4 cards trên desktop
     slidesToScroll: 1,
     arrows: true,
+    
     responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 4,  // vẫn giữ 4 ở các desktop nhỏ hơn nếu muốn
-        },
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,  // tablet
-        },
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,  // mobile
-        },
-      },
+      { breakpoint: 1200, settings: { slidesToShow: 4 } },
+      { breakpoint: 992,  settings: { slidesToShow: 2 } },
+      { breakpoint: 576,  settings: { slidesToShow: 1 } },
     ],
   };
 
@@ -38,11 +25,11 @@ export default function Home() {
       {/* Section 1: tiêu đề */}
       <section className="home-section">
         <div className="home-content">
-          <h1>HOÀNG GIA QUÁN <br />THẾ GIỚI CỦA TA</h1>
+          <h1>HOÀNG GIA QUÁN<br />THẾ GIỚI CỦA TA</h1>
         </div>
       </section>
 
-      {/* Section 2: Menu Món mới ra lò */}
+      {/* Section 2: Món mới ra lò */}
       <section className="new-menu-section">
         <h2 className="section-title">Món mới ra lò</h2>
         <div className="slider-wrapper">
@@ -63,6 +50,38 @@ export default function Home() {
         </div>
         <div className="btn-wrap">
           <button className="btn-view-menu">Xem thực đơn</button>
+        </div>
+      </section>
+
+      {/* Section 3: chữ chạy */}
+      <section className="promotion-section">
+        <div className="promotion-wrapper">
+          <span className="promotion-text">
+            ƯU ĐÃI HOÀNG GIA 🔖 ƯU ĐÃI HOÀNG GIA 🔖 ƯU ĐÃI HOÀNG GIA 🔖 ƯU ĐÃI HOÀNG GIA 🔖 ƯU ĐÃI HOÀNG GIA 🔖 ƯU ĐÃI HOÀNG GIA 🔖 ƯU ĐÃI HOÀNG GIA 🔖 ƯU ĐÃI HOÀNG GIA 🔖
+          </span>
+        </div>
+      </section>
+
+      {/* Section 4: Khuyến mãi & Tin tức */}
+      <section className="promo-cards-section">
+        {/* <h2 className="section-title">Ưu đãi &amp; Tin tức</h2> */}
+        <div className="slider-wrapper promo">
+          <Slider {...settings}>
+            {endows.map(item => (
+              <div key={item.id} className="card-item small">
+                <div className="card-image">
+                  <img src={item.image} alt={item.description} />
+                </div>
+                <div className="card-info">
+                  <h3>{item.description}</h3>
+                  <button className="btn-receive">Xem Ngay</button>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+        <div className="btn-wrap">
+          <button className="btn-view-all">Xem tất cả</button>
         </div>
       </section>
     </>
