af39c656fb629390188309e7440766c75d1fcce5
nguyennga <nguyennga2617@gmail.com>
2025-06-20 21:34:49 +0700

    nga lây api tao don dat ban

--- src/helpers/admin/api_helper.jsx
+++ src/helpers/admin/api_helper.jsx
@@ -5,7 +5,7 @@ import accessToken from "./jwt-token-access/accessToken";
 const token = accessToken;
 
 //apply base url for axios
-const API_URL = "";
+const API_URL = "http://127.0.0.1:8000/api";
 
 const axiosApi = axios.create({
   baseURL: API_URL,
